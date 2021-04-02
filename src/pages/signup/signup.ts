a43011e5f295bcbefaef219813ed26383e2ac8af
Theodosis Melidis <theodosios.melidis@intrasoft-intl.com>
2021-04-02 20:08:30 +0300

    feat: signup page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,29 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
+import firebase from "firebase";
 
 @Component({
   selector: "page-signup",
   templateUrl: "signup.html",
 })
 export class SignupPage {
+  name: string = "";
+  email: string = "";
+  password: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  signup() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.email, this.password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   goBack() {
     this.navCtrl.pop();
